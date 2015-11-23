7b6c4a8c7a5d6b3c54ee98ebcca023b179f3070c
Jake B <jake.booher@gmail.com>
2015-11-23 10:54:57 -0600

    Initial Commit

---
+++ library/ThemeHouse/ReportsNode/Install/Controller.php
@@ -0,0 +1,7 @@
+<?php
+
+class ThemeHouse_ReportsNode_Install_Controller extends ThemeHouse_Install
+{
+    
+    protected $_resourceManagerUrl = 'http://xenforo.com/community/resources/node-name-in-reports-list-by-th.3323/';
+}
---
+++ library/ThemeHouse/ReportsNode/Listener/LoadClass.php
@@ -0,0 +1,5 @@
+<?php
+
+class ThemeHouse_ReportsNode_Listener_LoadClass extends ThemeHouse_Listener_LoadClass
+{
+}
